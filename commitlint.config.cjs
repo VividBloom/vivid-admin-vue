ca1ddf2dddb09b07026d6e5d513590f6dd27ecff
blues <singthi@163.com>
2026-01-18 06:56:45 +0800

    fix: 修改commitlint配置

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -6,14 +6,18 @@ module.exports = {
       'always',
       [
         'feat', // 新功能
-        'fix', // 修复Bug
+        'fix', // 修复bug
         'docs', // 文档更新
         'style', // 代码格式调整
         'refactor', // 代码重构
-        'perf', // 性能优化
         'test', // 测试相关
         'chore', // 构建过程或辅助工具变动
+        'init', // 初始化（你已正确添加）
       ],
     ],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 72],
   },
 }
